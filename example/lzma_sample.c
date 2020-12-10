fe9333cec3d031aa6308018f1f4d14e0f73a110b
chenjiafan <chenjiafan@rt-thread.com>
2020-12-10 15:06:31 +0800

    [add] add lzmainfo function

--- example/lzma_sample.c
+++ example/lzma_sample.c
@@ -207,11 +207,87 @@ _exit:
 
     return ret;
 }
-MSH_
 
-static void lzmainfo(int argc, char *argv[])
+int log_2(UInt32 num)
 {
+    int count = 0;
+    while(num)
+    {
+        num = num >> 1;
+        if(num != 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
+static int lzmainfo(int argc, char *argv[])
+{
+    CLzmaProps props;
+    UInt32 dic_size;
+    Byte props_buff[LZMA_PROPS_SIZE + sizeof(UInt64)];
+    SizeT dcmprs_size = 0;
+    Byte d;
+    int fd_in = -1;
+    int ret = 0;
+
+    if(argc != 2)
+    {
+        help();
+        ret = -1;
+        goto _exit;
+    }
+
+    fd_in = open(argv[1], O_RDONLY, 0);
+    if(fd_in < 0)
+    {
+        rt_kprintf("[lzma] open the lzma file: %s erroe\n", argv[1]);
+        ret = -1;
+        goto _exit;
+    }
+
+    read(fd_in, props_buff, LZMA_PROPS_SIZE + sizeof(UInt64));
+
+    for(int i = 0; i < 8; i++)
+        dcmprs_size += (UInt64)props_buff[LZMA_PROPS_SIZE + i] << (i * 8);
+
+    dic_size = props_buff[1] | ((UInt32)props_buff[2] << 8) |
+              ((UInt32)props_buff[3] << 16) | ((UInt32)props_buff[4] << 24);
+
+    if(dic_size < (1 << 12))
+    {
+        dic_size = (1 << 12);
+    }
+
+    props.dicSize = dic_size; 
+
+    d = props_buff[0];
+
+    if(d >= (9 * 5 * 5))
+    {
+        rt_kprintf("lzmainfo: %s: Not a lzma file\n", argv[1]);
+        goto _exit;
+    }
+
+    props.lc = (Byte)(d % 9);
+    d /= 9;
+    props.pb = (Byte)(d / 5);
+    props.lp = (Byte)(d % 5);
+
+    rt_kprintf("\n%s\n", argv[1]);
+    rt_kprintf("Uncompressed size:              %d MB (%d bytes)\n", (UInt32)(dcmprs_size / 1024 / 1024), dcmprs_size);
+    rt_kprintf("Dictionary size:                %d MB (2^%d bytes)\n", (UInt32)(props.dicSize / 1024 / 1024), log_2(props.dicSize));
+    rt_kprintf("Literal context bits (lc):      %d\n", props.lc);
+    rt_kprintf("Literal pos bits (lp):          %d\n", props.lp);
+    rt_kprintf("Number of pos bits (pb):        %d\n\n", props.pb);
+
+_exit:
+    if(fd_in >= 0)
+    {
+        close(fd_in);
+    }
+    return 0;
 }
 
 #ifdef RT_USING_FINSH
@@ -219,7 +295,7 @@ static void lzmainfo(int argc, char *argv[])
 
 #include <finsh.h>
 
-MSH_CMD_EXPORT_ALIAS(lzma, lzma compress and decompress test);
-MSH_CMD_EXPORT_ALIAS(lzmainfom, Displays information about the LZMA compression package);
+MSH_CMD_EXPORT(lzma, lzma compress and decompress test);
+MSH_CMD_EXPORT(lzmainfo, Displays information about the LZMA compression package);
 #endif
 #endif
